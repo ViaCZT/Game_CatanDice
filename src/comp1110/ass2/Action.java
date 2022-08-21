1f4562091e8211a080a361767a54d3586d543b36
ZiHan Ai <u7528678@anu.edu.au>
2022-08-21 17:27:31 +1000

    Action

---
+++ src/comp1110/ass2/Action.java
@@ -0,0 +1,14 @@
+package comp1110.ass2;
+
+public class Action {
+    // Actions player can do during a turn
+    // build is put a road,settlement,city,knight on the board
+    // trade is use gold to get other resources
+    // swap is use knight to change the resources the player get
+    public enum ActionsTypes{
+        build,trade,swap;
+    }
+
+
+
+}
